7daf1dddb06e9b0eb1d0633eae95fcf97eadd282
dineshkumarsathasivam1011 <sathasivamdineshkumar@gmail.com>
2026-06-04 08:11:23 +0530

    Initail commit for the azure sql DB

---
+++ DatabaseDemoApp/AzureDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DatabaseDemoApp;
+
+public class AzureDataAccess
+{
+    private readonly IConfiguration _configuration;
+
+    public AzureDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+
+    }
+
+    public async Task<List<T>> LoadData<T, U>(string storedProcedure, 
+    U Parameters, string conectionStringName = "default")
+    {
+        using IDbConnection connection = new SqlConnection(_configuration.GetConnectionString(conectionStringName));
+        List<T> rows = (await connection.QueryAsync<T>(storedProcedure, Parameters, commandType: CommandType.StoredProcedure)).ToList();
+        return rows;
+
+    }
+    
+        
+
+
+}
